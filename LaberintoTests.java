37c0f1f132db815f1204a0d4ec4df65c7b1d57a4
JoseCabrera05 <126153385+JoseCabrera05@users.noreply.github.com>
2023-06-13 17:30:02 +0200

    Update LaberintoTests.java

--- LaberintoTests.java
+++ LaberintoTests.java
@@ -1,209 +1,212 @@
 import java.util.Scanner;
 
-public class elLaberinto {
+public class laberintoFinal {
 
-    static int[][] elLaberinto = {
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-            { 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0 },
-            { 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0 },
-            { 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-            { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    static int[][] labyrinth = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+        {0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+        {0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
 
     static int spotX, spotY;
     static int limIX, limIY, limSX, limSY;
     static int torchRange = 3;
-    static int[] dioSpotX={5,9,4}, diospotY={13,8,6};
+    static int[] dioSpotX = { 10, 6, 4 }, dioSpotY = { 22, 4, 14 };
 
     public static void main(String[] args) {
+
         Scanner entrada = new Scanner(System.in);
-        spotX = 2;
+        spotX = 0;
         spotY = 0;
+
         limIX = 0;
         limIY = 0;
-        limSX = elLaberinto.length - 1;
-        limSY = elLaberinto[0].length - 1;
+        limSX = labyrinth.length - 1;
+        limSY = labyrinth[0].length - 1;
 
         boolean walk = true;
-        String action;
-        direction();
+        String accion;
+        impLab();
 
-        while (walk == true) {
+        while (walk) {
             commands();
-            System.out.print("Proceda a moverse. ");
-            action = entrada.nextLine();
+            System.out.print("Ingrese comando: ");
+            accion = entrada.nextLine();
 
-                 static void east() {
-                   if ((spotY < limSY) && (elLaberinto[spotX][spotY + 1] != 1)) {
-                        spotY = spotY + 1;
-                   } else {
-                       System.out.print("Camino bloqueado... ");
-                       }
-                  }
-
-                  static void west() {
-                      if ((spotY > limIY) && (elLaberinto[spotX][spotY - 1] != 1)) {
-                          spotY = spotY - 1;
-                      } else {
-                         System.out.print("No puedes ir más allá! ");
-                     }
-                  }
-
-                  static void south() {
-                      if ((spotX < limSX) && (elLaberinto[spotX + 1][spotY] != 1)) {
-                          spotX = spotX + 1;
-                      } else {
-                          System.out.print("No puedes ir más allá! ");
-                     }
-                 }
-
-                  static void north() {
-                      if ((spotX > limIX) && (elLaberinto[spotX - 1][spotY] != 1)) {
-                          spotX = spotX - 1;
-                      } else {
-                          System.out.print("No puedes ir más allá! ");
-                      }
-                  }
-                      }
-
-    
-
-    static void move(String direction) {
-        if (direction.equals("east")) {
-            if ((spotY < limSY) && (elLaberinto[spotX][spotY + 1] != 1)) {
-                spotY = spotY + 1;
-            } else {
-                System.out.print("Camino bloqueado... ");
+            for (int v = 0; v < 3; v++) {
+                dioSpotX[v] = newSpotX(dioSpotX[v], dioSpotY[v]);
+                dioSpotY[v] = newSpotY(dioSpotX[v], dioSpotY[v]);
             }
-        } else if (direction.equals("west")) {
-            if ((spotY > limIY) && (elLaberinto[spotX][spotY - 1] != 1)) {
-                spotY = spotY - 1;
+
+            if (accion.equals("w")) {
+                mover("north");
+            } else if (accion.equals("s")) {
+                mover("south");
+            } else if (accion.equals("a")) {
+                mover("west");
+            } else if (accion.equals("d")) {
+                mover("east");
+            } else if (accion.equals("f")) {
+                walk = false;
             } else {
-                System.out.print("Camino bloqueado...");
+                System.out.println(
+                        "Movimiento no reconocido, intente con un valor valido.");
             }
-        } else if (direction.equals("south")) {
-            if ((spotX < limSX) && (elLaberinto[spotX + 1][spotY] != 1)) {
-                spotX = spotX + 1;
-            } else {
-                System.out.print("Camino bloqueado...");
-            }
-        } else if (direction.equals("north")) {
-            if ((spotX > limIX) && (elLaberinto[spotX - 1][spotY] != 1)) {
-                spotX = spotX - 1;
-            } else {
-                System.out.print("Camino bloqueado...");
-            }
+            impLab();
         }
     }
 
-    for (int v=0;v<3;v=v+1){
-        dioSpotX[v] = newSpotX(dioSpotX[v], diospotY[v]);
-        diospotY[v] = newSpotY(dioSpotX[v], diospotY[v]);
-    }
 
-    static void direction() {
+    static void mover(String directions) {
+        switch (directions) {
+            case "east":
+                if ((spotY > limIY) && (labyrinth[spotX][spotY - 1] != 1)) {
+                    spotY = spotY - 1;
+                } else {
+                    System.out.print("Eso es un muro... ");
+                }
+                break;
+            case "west":
 
-        printLabyrinth();
-    }
-
-    static void nulo()
-
-    {
-        System.out.print("movement invalido.");
-
+                if ((spotY < limSY) && (labyrinth[spotX][spotY + 1] != 1)) {
+                    spotY = spotY + 1;
+                } else {
+                    System.out.print("No puedes atravesar paredes... aun. ");
+                }
+                break;
+            case "south":
+                if ((spotX < limSX) && (labyrinth[spotX + 1][spotY] != 1)) {
+                    spotX = spotX + 1;
+                } else {
+                    System.out.print("Te vas a lastimar si sigues asi. ");
+                }
+                break;
+            case "north":
+                if ((spotX > limIX) && (labyrinth[spotX - 1][spotY] != 1)) {
+                    spotX = spotX - 1;
+                } else {
+                    System.out.print("Por ahi no es. ");
+                }
+                break;
+            default:
+                System.out.print("Dirección no válida");
+                break;
+        }
     }
 
     static void commands() {
-        System.out.println("CMDs: [W]north,[S]south,[A]west,[D]east,[F]Fin");
+        System.out.println("Comandos: [w]=Norte,[s]=Sur,[a]=Este,[d]=Oeste,[f]=Finalizar tu viaje");
     }
 
-    static void printLabyrinth() {
-        int i, j;
+    static void impLab() {
+
+        String factor = "";
         System.out.println("");
         System.out.println("+----------------------------------------------------------+");
-        for (i = 0; i < (elLaberinto.length); i = i + 1) {
+        for (int i = 0; i < (labyrinth.length); i++) {
             System.out.print("|");
-            for (j = 0; j < (elLaberinto[i].length); j = j + 1) {
-
+            for (int j = 0; j < (labyrinth[i].length); j++) {
+                factor = "  ";
                 if ((i == spotX) && (j == spotY)) {
-                    System.out.print("A");
+                    factor = "HM";
                 } else {
-                    if (elLaberinto[i][j] == 1) {
-                        System.out.print("X");
+
+                    if ((spotX + torchRange >= i) && (spotX - torchRange <= i) && (spotY + torchRange >= j)
+                            && (spotY - torchRange <= j)) {
+                        if (labyrinth[i][j] == 1) {
+                            factor = "[]";
+                        } else if (labyrinth[i][j] == 2) {
+                            factor = "/|";
+                        } else if (labyrinth[i][j] == 5) {
+                            factor = "/\\";
+                        } else if (labyrinth[i][j] == 9) {
+                            factor = "O-";
+                        } else {
+                            factor = "··";
+                        }
+                        ;
+                        for (int v = 0; v < 3; v = v + 1) {
+                            if ((i == dioSpotX[v]) && (j == dioSpotY[v])) {
+                                factor = "^^";
+                            }
+                        }
                     } else {
-                        System.out.print("  ");
+                        factor = "  ";
                     }
-                    ;
+
                 }
+                System.out.print(factor);
             }
-                if((spotX+torchRange>=i)&&(spotX-torchRange<=i)&&(spotY+torchRange>=j)&&(spotY-torchRange<=j))
-                {
-                    if(elLaberinto[i][j]==1)        {factor ="[]";}
-                    else if(elLaberinto[i][j]==2)   {factor ="/|";}
-                    else if(elLaberinto[i][j]==5)   {factor ="/\\";}
-                    else if(elLaberinto[i][j]==9)   {factor ="O-";}
-                    else {factor ="··";};
-                    for (int v=0;v<3;v=v+1){
-                        if((i==dioSpotX[v])&&(j==diospotY[v])) {factor ="^^";}
-                    }
-                }
-                else
-                {
-                    factor ="  ";
-                }
-
-  
-    System.out.print(factor );
-
-    
-
-    private int newSpotY(int i, int i0) {
-        
-    }
-            }
-        System.out.println("|----------------------------------------------------------|");
-        System.out.println("| A |                                                      |");
+            System.out.print("|");
+            System.out.println();
+        }
         System.out.println("+----------------------------------------------------------+");
     }
-    static int movement(){
-        double movementChance=Math.random();
-        if (movementChance<0.40) {return 1;}
-            else if(movementChance<0.80) {return -1;}
-                else {return 0;}
+
+
+    static int movement() {
+        double movementChance = Math.random();
+        if (movementChance < 0.40) {
+            return 1;
+        } else if (movementChance < 0.80) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
-    static int newSpotX(int oldSpotX, int oldSpotY){
+    static int newSpotX(int oldSpotX, int oldSpotY) {
         int variable = movement();
         int newSpotX = oldSpotX;
-        if((oldSpotX!=minX&&oldSpotX!=maxX)){
-            if (elLaberinto[oldSpotX+variable][oldSpotY]!=1){
-                newSpotX = oldSpotX + variable;
+
+        if (variable == 1) {
+            if ((oldSpotX != limIX) && (oldSpotX != limSX)) {
+                if (labyrinth[oldSpotX + 1][oldSpotY] != 1) {
+                    newSpotX = oldSpotX + 1;
+                }
+            }
+        } else if (variable == -1) {
+            if ((oldSpotX != limIX) && (oldSpotX != limSX)) {
+                if (labyrinth[oldSpotX - 1][oldSpotY] != 1) {
+                    newSpotX = oldSpotX - 1;
+                }
             }
         }
+
         return newSpotX;
     }
 
-    static int nuevaPosY(int oldSpotX, int oldSpotY){
+    static int newSpotY(int oldSpotX, int oldSpotY) {
         int variable = movement();
-        int newPosY = oldSpotY;
-        if((oldSpotY!=minY&&oldSpotY!=maxY)){
-            if (elLaberinto[oldSpotX][oldSpotY+variable]!=1){
-                newPosY = oldSpotY + variable;
+        int newSpotY = oldSpotY;
+
+        if (variable == 1) {
+            if ((oldSpotY != limIY) && (oldSpotY != limSY)) {
+                if (labyrinth[oldSpotX][oldSpotY + 1] != 1) {
+                    newSpotY = oldSpotY + 1;
+                }
+            }
+        } else if (variable == -1) {
+            if ((oldSpotY != limIY) && (oldSpotY != limSY)) {
+                if (labyrinth[oldSpotX][oldSpotY - 1] != 1) {
+                    newSpotY = oldSpotY - 1;
+                }
             }
         }
-        return newPosY;
+
+        return newSpotY;
     }
-            System.out.print("|");
-            System.out.println();
 
 }
